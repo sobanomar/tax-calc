8012a2f754a74d432cb5f30aa4d022fd74dfb306
Soban Umar <sobanomr@gmail.com>
2023-12-23 18:58:14 +0500

    removed extra fields

--- src/components/DataField.js
+++ src/components/DataField.js
@@ -4,34 +4,37 @@ import { formatPropVal } from "../Utils/FormatPropertyValue";
 
 const DataField = ({ name, value }) => {
   return (
-    <View
-      style={{
-        padding: 10,
-        marginVertical: 5,
-        backgroundColor: "rgb(224 224 255)",
-        borderRadius: 5,
-        flexDirection: "row",
-      }}
-    >
-      <Text
+    name !== "Preferred Tax liability" &&
+    name !== "Current Tax Liability" && (
+      <View
         style={{
-          fontSize: 17,
-          width: "55%",
+          padding: 10,
+          marginVertical: 5,
+          backgroundColor: "rgb(224 224 255)",
+          borderRadius: 5,
+          flexDirection: "row",
         }}
       >
-        {name}:
-      </Text>
-      <Text
-        style={{
-          fontSize: 17,
-          width: "45% ",
-        }}
-      >
-        {name === "Property Value" || name === "Rent Value"
-          ? formatPropVal(value)
-          : value}
-      </Text>
-    </View>
+        <Text
+          style={{
+            fontSize: 17,
+            width: "55%",
+          }}
+        >
+          {name}:
+        </Text>
+        <Text
+          style={{
+            fontSize: 17,
+            width: "45% ",
+          }}
+        >
+          {name === "Property Value" || name === "Rent Value"
+            ? formatPropVal(value)
+            : value}
+        </Text>
+      </View>
+    )
   );
 };
 

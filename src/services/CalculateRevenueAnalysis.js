e8920a2cf49520d2fc95874e6a60adfbe3cb02a5
talha-ayub-qbatch <talha.ayub@qbatch.com>
2023-12-26 21:12:46 +0500

    Replace fetch with axio & add config for HTTP

--- src/services/CalculateRevenueAnalysis.js
+++ src/services/CalculateRevenueAnalysis.js
@@ -1,28 +1,23 @@
+import axios from 'axios';
+
 const url = "http://18.205.239.27:5000/analysis";
 
-// Assuming finalData.current is your data
 export const calculateRevenueAnalysis = async (finalData) => {
-  // const data = finalData;
-
   try {
-    // Make a POST request
     console.log("Calculating...");
-    const response = await fetch(url, {
-      method: "POST",
+    const response = await axios.post(url, finalData, {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         // Add any other headers if needed
       },
-      body: JSON.stringify(finalData),
     });
 
     // Check if the request was successful
-    if (!response.ok) {
+    if (response.status !== 200) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    // Parse and return the JSON data
-    // const jsonData = await response.json();
+    // Return the Axios response object
     return response;
   } catch (error) {
     console.error("Error :", error.message);
